bac5d7a1ef3ba96d39dce72c7b898dabc225f37f
Jioo Jung <64303211+jioome@users.noreply.github.com>
2022-01-30 17:02:26 +0900

    greedy

--- Algorithm/Greedy/조이스틱.py
+++ Algorithm/Greedy/조이스틱.py
@@ -1,15 +1,30 @@
 def solution(name):
     answer = 0
-    print(answer)
-    # for n in name : 
-    #     print(n)
-    #     if abs(ord(n)-ord('A'))<= len(name)//2 :
-    #         answer +=  abs(ord(n)-ord('A'))
-    #         print(ord(n))
-    #     else : 
-    #         answer += (len(name)+ord('A')-ord(n))
-    # print(answer)
-    return answer
+    n = len(name)
 
-solution("JEROEN")
-print(111)
+    def alphabet_to_num(char):
+        num_char = [i for i in range(14)] + [j for j in range(12, 0, -1)]
+        return num_char[ord(char) - ord('A')]
+    
+    # 위 아래 조작 
+    for ch in name:
+        answer += alphabet_to_num(ch)
+
+    move = n - 1
+    # 커서 좌우 이동 
+    for idx in range(n):
+        next_idx = idx + 1
+
+        while (next_idx < n) and (name[next_idx] == 'A'):
+            next_idx += 1
+        
+        # idx 에서 되돌아 가는 경우 /  뒤에 있는 문자열부터 갔다오는 경우 ex) BBCAAAN
+        distance = min(idx, n - next_idx)
+
+        # 한 방향으로만 이동하는 경우와, 오른쪽으로 이동했다가 왼쪽으로 이동하는 경우를 비교
+        move = min(move, idx  + distance+ n - next_idx)
+
+    answer += move
+
+    return answer
+solution("JAAAAAAN")
