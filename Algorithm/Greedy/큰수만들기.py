ef9d04cab478cc5570d3c3fc3ec7f37ef531cd73
Jioo Jung <64303211+jioome@users.noreply.github.com>
2022-02-03 09:37:37 +0900

    greedy

--- Algorithm/Greedy/큰수만들기.py
+++ Algorithm/Greedy/큰수만들기.py
@@ -4,6 +4,7 @@ def solution(number, k):
     for n in number : 
         while answer and answer[-1] < n and k>0 : 
             answer.pop()
+            k-=1
         answer.append(n)
     answer = ''.join(answer[:len(number)-k])
     print(answer)
