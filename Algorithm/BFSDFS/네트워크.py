9d6edc1200532246eb12f1f444d181fbca9f46bf
Jioo Jung <64303211+jioome@users.noreply.github.com>
2022-02-19 14:19:42 +0900

    DFS

---
+++ Algorithm/BFSDFS/네트워크.py
@@ -0,0 +1,19 @@
+def dfs(n,computers,start,visited):
+    visited[start] = True
+    for i in range(n):
+        if visited[i] == False and computers[start][i] == 1 :
+            visited = dfs(n,computers, i,visited)
+    return visited
+
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n 
+
+    for start in range(n):
+        if visited[start] == False : 
+            dfs(n,computers,start,visited)
+
+    return answer
+
+solution(3,	[[1, 1, 0], [1, 1, 0], [0, 0, 1]])
