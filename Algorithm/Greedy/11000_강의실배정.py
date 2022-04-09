27bddcbe46635fc4aa691ab1eeba19f79545257d
Jioo Jung <64303211+jioome@users.noreply.github.com>
2022-04-09 14:38:21 +0900

    [BOJ]강의실배정

--- Algorithm/Greedy/11000_강의실배정.py
+++ Algorithm/Greedy/11000_강의실배정.py
@@ -1,23 +1,19 @@
-import heapq
+import heapq,sys
 time = [] 
 room = []
-lecture = 0 
+input = sys.stdin.readline # 시간초과 문제 
 n = int(input())
 for i in range(n):
     time.append(list(map(int,input().split())))
 time.sort()
-for s,t in time : 
-    if len(room) == 0 : 
-        heapq.heappush(room,t)
-        lecture += 1
-        continue 
-    elif room[0] <= s :
+heapq.heappush(room,time[0][1])
+for i in range(1,len(time)) : 
+    if room[0] <= time[i][0] :
         heapq.heappop(room)
-        heapq.heappush(room,t)
+        heapq.heappush(room,time[i][1])
     else : 
-        heapq.heappush(room,t)
-        lecture += 1 
-print(lecture)
+        heapq.heappush(room,time[i][1])
+print(len(room))
      
 
 
